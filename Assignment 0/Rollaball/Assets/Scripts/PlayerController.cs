ac43114d5b3e6b24dd09277cd6494f39a65dfbc3
francisvtran <francisvtran@gmail.com>
2024-01-30 15:13:12 -0600

    Created Collectibles

--- Assignment 0/Rollaball/Assets/Scripts/PlayerController.cs
+++ Assignment 0/Rollaball/Assets/Scripts/PlayerController.cs
@@ -31,5 +31,13 @@ public class PlayerController : MonoBehaviour
 
         rb.AddForce(movement * speed);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
 }
 
